b1fe203214c97233462858d1f440ff97e7387f05
Farhadam <teacherfarhad.net@gmail.com>
2025-06-03 16:23:42 +0330

    Episode 96 Complate Create Tables With Ado Net

--- DataLayer.ADO/AdoConnection.cs
+++ DataLayer.ADO/AdoConnection.cs
@@ -5,15 +5,16 @@ public class AdoConnection
     string connectionString = "Server=.;Integrated Security=true;";
     string databaseName = "AccountingDB";
     string useMyDataBase = $"Use AccountingDB";
-    string createPersonCategoryTable = "Create Table PersonCategories ( Id int Not Null Primary Key Identity, Title Nvarchar(250) Not NULL ); Create Unique Index PersonCategory_Title_Index  On PersonCategories(Title);";
-    string createPeopleTable = "Create Table People ( Id int Not Null Primary Key Identity, FullName Nvarchar(250) Not NULL, Mobile Nvarchar(11) Not NULL, Email Nvarchar(250) NULL, BirthDate DateTime NULL, CreateDate DateTime DEFAULT GETDATE(), PersonCategoryId int Not Null , Constraint FK_PersonCategory  Foreign Key (PersonCategoryId) References PersonCategories(Id), Constraint Check_Mobile_Length Check(Len(Mobile) = 11) ); Create Unique Index People_Mobile_Index  On People(Mobile); Create Unique Index People_Email_Index  On People(Email) Where Email Is Not NULL;";
-    string createSlariesTable = "Create Table Salaries ( Id int Not Null Identity Primary Key, PersonId int Not Null , Amount int Not NULL, Description NVARCHAR(MAX) Not null, DepositDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(), Constraint FK_Person  Foreign Key (PersonId) References People(Id),);";
-    string createExpenseCategoriesTable = "Create Table ExpenseCategories ( Id int Not Null Identity Primary Key, Title NVarchar(250) Not Null , ParentId int Null, Constraint FK_ExpenseCategory_Child Foreign Key (ParentId) References ExpenseCategories(Id), );  Create Unique Index ExpenseCategories_Title_Index  On ExpenseCategories(Title);";
-    string createExpensesTable = "Create Table Expenses ( Id int Not Null Identity Primary Key, ExpenseCategoryId int Not Null, Amount int Not NULL, Description NVARCHAR(MAX) Not null, ExpenseDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(),      Constraint FK_ExpenseCategory  Foreign Key (ExpenseCategoryId) References ExpenseCategories(Id),  );";
-    string createChequesTable = "Create Table Cheques ( Id int Not Null Identity Primary Key, PersonId int Not Null , Amount int Not NULL, Description NVARCHAR(MAX) Not null, BankName NVARCHAR(150) Not null, ChequeDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(), Status int Not Null ,     Constraint FK_Person_Cheque  Foreign Key (PersonId) References People(Id),  \tConstraint Check_Status_Value Check(Status = 0 Or Status = 1) );";
+    string createPersonCategoryTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'PersonCategories' AND type = 'U') BEGIN Create Table PersonCategories ( Id int Not Null Primary Key Identity, Title Nvarchar(250) Not NULL ); Create Unique Index PersonCategory_Title_Index  On PersonCategories(Title); END";
+    string createPeopleTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'People' AND type = 'U') BEGIN Create Table People ( Id int Not Null Primary Key Identity, FullName Nvarchar(250) Not NULL, Mobile Nvarchar(11) Not NULL, Email Nvarchar(250) NULL, BirthDate DateTime NULL, CreateDate DateTime DEFAULT GETDATE(), PersonCategoryId int Not Null , Constraint FK_PersonCategory  Foreign Key (PersonCategoryId) References PersonCategories(Id), Constraint Check_Mobile_Length Check(Len(Mobile) = 11) ); Create Unique Index People_Mobile_Index  On People(Mobile); Create Unique Index People_Email_Index  On People(Email) Where Email Is Not NULL; END";
+    string createSlariesTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Salaries' AND type = 'U') BEGIN Create Table Salaries ( Id int Not Null Identity Primary Key, PersonId int Not Null , Amount int Not NULL, Description NVARCHAR(MAX) Not null, DepositDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(), Constraint FK_Person  Foreign Key (PersonId) References People(Id),); END";
+    string createExpenseCategoriesTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'ExpenseCategories' AND type = 'U') BEGIN Create Table ExpenseCategories ( Id int Not Null Identity Primary Key, Title NVarchar(250) Not Null , ParentId int Null, Constraint FK_ExpenseCategory_Child Foreign Key (ParentId) References ExpenseCategories(Id), );  Create Unique Index ExpenseCategories_Title_Index  On ExpenseCategories(Title); END";
+    string createExpensesTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Expenses' AND type = 'U') BEGIN Create Table Expenses ( Id int Not Null Identity Primary Key, ExpenseCategoryId int Not Null, Amount int Not NULL, Description NVARCHAR(MAX) Not null, ExpenseDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(),      Constraint FK_ExpenseCategory  Foreign Key (ExpenseCategoryId) References ExpenseCategories(Id),); END";
+    string createChequesTable = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Cheques' AND type = 'U') BEGIN Create Table Cheques ( Id int Not Null Identity Primary Key, PersonId int Not Null , Amount int Not NULL, Description NVARCHAR(MAX) Not null, BankName NVARCHAR(150) Not null, ChequeDate DateTime2 Not Null, CreateDate DateTime DEFAULT GETDATE(), Status int Not Null ,     Constraint FK_Person_Cheque  Foreign Key (PersonId) References People(Id),  \tConstraint Check_Status_Value Check(Status = 0 Or Status = 1) ); END";
     public AdoConnection()
     {
         CreateDataBase();
+        CreateTables();
     }
     public bool CreateDataBase()
     {
@@ -34,40 +35,33 @@ public class AdoConnection
             return false;
         }
     }
-    public void CreateTables()
+    public bool CreateTables()
     {
+        List<string> Queries = new List<string>()
+        {
+            useMyDataBase,
+            createPersonCategoryTable,
+            createPeopleTable,
+            createSlariesTable,
+            createExpenseCategoriesTable,
+            createExpensesTable,
+            createChequesTable
+        };
         try
         {
             SqlConnection connection = new SqlConnection(connectionString);
-           
-            SqlCommand command1 = new SqlCommand(useMyDataBase, connection);
             connection.Open();
-            command1.ExecuteNonQuery();
-            Console.WriteLine("Use DataBase Success");
-            SqlCommand command2 = new SqlCommand(createPersonCategoryTable, connection);
-            command2.ExecuteNonQuery();
-            Console.WriteLine("create PersonCategory Table Success");
-            SqlCommand command3 = new SqlCommand(createPeopleTable, connection);
-            command3.ExecuteNonQuery();
-            Console.WriteLine("create People Table Success");
-            SqlCommand command4 = new SqlCommand(createSlariesTable, connection);
-            command4.ExecuteNonQuery();
-            Console.WriteLine("create Slaries Table");
-            SqlCommand command5 = new SqlCommand(createExpenseCategoriesTable, connection);
-            command5.ExecuteNonQuery();
-            Console.WriteLine("create Expense Categories Table");
-            SqlCommand command6 = new SqlCommand(createExpensesTable, connection);
-            command6.ExecuteNonQuery();
-            Console.WriteLine("create Expenses Table Success");
-            SqlCommand command7 = new SqlCommand(createChequesTable, connection);
-            command7.ExecuteNonQuery();
-            Console.WriteLine("create Cheques Table Success");
+            foreach(var  query in Queries)
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
             connection.Close();
-            Console.WriteLine("All Tables Created Successfully");
+            return true;
         }
-        catch (Exception ex)
+        catch 
         {
-            Console.WriteLine(ex.Message.ToString());   
+            return false;
         }
     }
 }
